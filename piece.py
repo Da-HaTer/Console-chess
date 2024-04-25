2c03cdb0db6ba0a11e1b12039b8a0b6dadf00ab5
Da-HaTer <oussamah4boubi@gmail.com>
2024-04-25 04:44:58 +0100

    added bishop moves and captures

--- piece.py
+++ piece.py
@@ -99,9 +99,9 @@ class ChessPiece:
         return new_pos
 
     def knight_move(self, white, position, move):
-        knight=[(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+        knight_moves=[(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
         
-        def get_knight(pos,i,j,symbol,ambiguous='',moves=knight):
+        def get_knight(pos,i,j,symbol,ambiguous='',moves=knight_moves):
             found=0
             for move in moves:
                 if 0<=i+move[0]<8 and 0<=j+move[1]<8 and pos[i+move[0],j+move[1]]==symbol: #if knight is found
@@ -214,7 +214,187 @@ class ChessPiece:
         else:
             raise InvalidMoveError(move)
         return new_pos
-        
+    
+    def bishop_move(self, white, position, move):
+        new_pos=position.copy()
+        bishop_moves=[(1,1),(-1,1),(1,-1),(-1,-1)]
+        def get_bishop(pos,i,j,symbol,ambiguous='',moves=bishop_moves):
+            found=0
+            for move in moves:
+                for k in range(1,8): 
+                    if 0<=i+k*move[0]<8 and 0<=j+k*move[1]<8: # all diagonal squares within bounds
+                        if pos[i+k*move[0],j+k*move[1]]==symbol: #if bishop is found
+                            ###add ambiguity later
+                            if ambiguous: #if ambiguity is specified
+                                if len(ambiguous)==1:
+                                    if ambiguous in 'abcdefgh':
+                                        if chr(j+k*move[1]+97)==ambiguous: # if column is specified
+                                            coords=(i+k*move[0],j+k*move[1]) #target square
+                                            found+=1
+                                    elif ambiguous in '12345678': # if row is specified
+                                        if 8-(i+k*move[0])==int(ambiguous): #if row is specified
+                                            coords=(i+k*move[0],j+k*move[1])
+                                            found+=1
+                                elif len(ambiguous)==2: #if both row and column are specified
+                                    if chr(j+k*move[1]+97)==ambiguous[0] and 8-(i+k*move[0])==int(ambiguous[1]):
+                                        print(i,j,k,move,ambiguous)
+                                        coords=(i+k*move[0],j+k*move[1])
+                                        found+=1
+                            #check if no pieces are between the bishop and the target square
+                            ct=False # skip outer loop if piece in way
+                            for l in range(1,k):# from target to bishop go k times diagonally
+                                if pos[i+l*move[0],j+l*move[1]]!='': #if piece in way
+                                    print("k:",k)
+                                    # raise IllegalMoveError(move)
+                                    ct=True
+                                    break #skip this bishop and direction for now
+                            if ct:
+                                continue
+                            if not ambiguous: # ex: Bf3
+                                found+=1
+                                coords=(i+k*move[0],j+k*move[1])
+                            if found>1:
+                                raise ambigousMoveError(move)
+            if found==1:
+                return coords
+            print("found",found)
+            raise IllegalMoveError(move)
+        if re.fullmatch(r'[b][a-h][1-8]', move,re.IGNORECASE): #simple bishop move: Bf3, Bf6, Bh3, Bh6
+            i=8-int(move[2])
+            j=ord(move[1])-ord('a')
+            if white:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'b')
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move) ###simple move can be implicit capture move
+            else:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'B')
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        elif re.match(r'[b][x][a-h][1-8]', move,re.IGNORECASE): #bishop capture move: Bxf3, Bxf6, Bxh3, Bxh6
+            i=8-int(move[3])
+            j=ord(move[2])-ord('a')
+            if white:
+                if new_pos[i,j] in 'PRNBQ':
+                    search=get_bishop(new_pos,i,j,'b')
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+            else:
+                if new_pos[i,j] in 'prnbq':
+                    search=get_bishop(new_pos,i,j,'B')
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        elif re.match(r'[b][a-h1-8][a-h][1-8]', move,re.IGNORECASE): #ambiguous bishop move: B2f3, Bcf3, B1f3, Bdf3
+            i=8-int(move[3])
+            j=ord(move[2])-ord('a')
+            a=move[1].lower()
+            if white:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'b',a)
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+            else:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'B',a)
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        elif re.match(r'[b][a-h][1-8][a-h][1-8]', move,re.IGNORECASE): #ambiguous bishop move: Bf5e5, Bd3c4, bd3c4, Bd3f5
+            i0=8-int(move[2])
+            j0=ord(move[1])-ord('a')
+            i=8-int(move[4])
+            j=ord(move[3])-ord('a')
+            if white:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'b',move[1:3])
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+            else:
+                if new_pos[i,j]=='':
+                    search=get_bishop(new_pos,i,j,'B',move[1:3])
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        elif re.match(r'[b][a-h1-8][x][a-h][1-8]', move,re.IGNORECASE): #ambiguous bishop capture move: B2xf3, Bcxf3, B1xf3, Bdxf3
+            i=8-int(move[4])
+            j=ord(move[3])-ord('a')
+            a=move[1].lower()
+            if white:
+                if new_pos[i,j] in 'PRNBQ':
+                    search=get_bishop(new_pos,i,j,'b',a)
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+            else:
+                if new_pos[i,j] in 'prnbq':
+                    search=get_bishop(new_pos,i,j,'B',a)
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        elif re.match(r'[b][a-h][1-8][x][a-h][1-8]', move,re.IGNORECASE): #ambiguous bishop capture move: Bf5xe5, Bd3xc4, bd3xc4, Bd3xf5
+            i0=8-int(move[2])
+            j0=ord(move[1])-ord('a')
+            i=8-int(move[5])
+            j=ord(move[4])-ord('a')
+            if white:
+                if new_pos[i,j] in 'PRNBQ':
+                    search=get_bishop(new_pos,i,j,'b',move[1:3])
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='b'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+            else:
+                if new_pos[i,j] in 'prnbq':
+                    search=get_bishop(new_pos,i,j,'B',move[1:3])
+                    if search:
+                        i0,j0=search
+                        new_pos[i,j]='B'
+                        new_pos[i0,j0]=''
+                else:
+                    raise IllegalMoveError(move)
+        else:
+            raise InvalidMoveError(move)
+        return new_pos
+                            
     def capture(self, position):
         # Implement the logic for capturing an opponent's piece
         pass
@@ -225,31 +405,45 @@ class ChessPiece:
 
 if __name__=="__main__":
     start_board= np.array([
-        ['R','N','B','Q','K','B','','R'],
+        ['R','N','B','Q','K','B','N','R'],
         ['P','P','P','P','P','P','P','P'],
         ['','','','','','','',''],
         ['','','','','','','',''],
-        ['','n','','','','n','',''],
+        ['','','','','','','',''],
         ['','','','','','','',''],
         ['p','p','p','p','p','p','p','p'],
-        ['r','','b','q','k','b','','r']
+        ['r','n','b','q','k','b','n','r']
     ])
 old_pos=start_board
 pos=np.copy(old_pos)
 white=True
 while True:
     board=Display(pos)
-    print(f'{"white" if white else board.colored("black")} to play')
-    move=input("Enter move: \n")
+    w='\033[1m'+"white"+'\033[0m' # bold repr
+    print(f'{ w if white else board.colored("black")} to play')
+    move=input("Enter move: [move]|back|skip \n")
     if move=="q":
         break
     elif move=="back":
         pos=old_pos
         white=not white
         continue
+    elif move=="skip":
+        white=not white
+        continue
     piece=ChessPiece('w',start_board)
     try:
-        position=piece.knight_move(white,pos,move)
+        ### conflict between b pawn and bishop notation should probably be resolved with classifying by syntax instead of throwing errors right away
+        ##example: bxc4 could be a pawn move or a bishop move
+        ## solution: bishop: capital letter, pawn: small letter b
+        if move[0] in 'abcdefgh':  
+            position=piece.pawn_move(white,pos,move)
+        elif move[0].lower() == 'n':
+            position=piece.knight_move(white,pos,move)
+        elif move[0] == 'B':
+            position=piece.bishop_move(white,pos,move)
+        else:
+            raise InvalidMoveError(move)
         board=Display(position)
         old_pos=np.copy(pos)
         pos=position
